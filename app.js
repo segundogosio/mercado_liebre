98795f159565825a6062126079fd1e7d435372ba
segundogosio <segundogosio@gmail.com>
2021-11-03 19:45:47 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require ("path");
+
+app.listen(3000, ()=>
+console.log("funciona"));
+
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, "./views/home.html"))
+});
+
+app.get("/registrer", (req, res) => {
+res.sendFile(path.join(__dirname, "./views/registrer.html"))
+});
